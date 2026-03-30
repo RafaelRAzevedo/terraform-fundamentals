c7c0d6e533cfacd73156982890ff5e611283708c
Rafael Azevedo <razevedo@realnetworks.com>
2026-03-30 19:57:32 +0000

    Fix backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg_lab"
-    storage_account_name = "fmacademymystorage1234"
+    storage_account_name = "fmacademystorage1234"
     container_name       = "tfstate"
     key = "student-rafael.tfstate"
   }
