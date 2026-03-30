4eb82e215ac90356a864812b918a5670fa4c146f
Rafael Azevedo <razevedo@realnetworks.com>
2026-03-30 19:55:47 +0000

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "existing-backend-rg"
-    storage_account_name = "existingstorageaccount"
+    resource_group_name  = "rg_lab"
+    storage_account_name = "fmacademymystorage1234"
     container_name       = "tfstate"
     key = "student-rafael.tfstate"
   }
